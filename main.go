cce89ca2f9c307b2df5f18beaa59a486ad17052b
kweijack <kweijack.dev@gmail.com>
2024-10-13 18:52:51 +0800

    task to run, stop docker

--- main.go
+++ main.go
@@ -1,3 +1,58 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/dev6699/cube/task"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	fmt.Println("create a test container")
+
+	dc, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return err
+	}
+
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:12-alpine",
+		Env: []string{
+			"POSTGRES_USER=cube",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	ctx := context.Background()
+	result, err := d.Run(ctx)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+
+	time.Sleep(5 * time.Second)
+
+	result, err = d.Stop(ctx, result.ContainerId)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return nil
+}
---
+++ task/docker.go
@@ -0,0 +1,112 @@
+package task
+
+import (
+	"context"
+	"io"
+	"math"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-connections/nat"
+)
+
+type DockerResult struct {
+	Action      string
+	ContainerId string
+	Result      string
+}
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachSterr   bool
+	ExposedPorts  nat.PortSet
+	Cmd           []string
+	Image         string
+	Cpu           float64
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
+
+type Docker struct {
+	Client *client.Client
+	Config Config
+}
+
+func (d *Docker) Run(ctx context.Context) (*DockerResult, error) {
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, types.ImagePullOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy{
+		Name: d.Config.RestartPolicy,
+	}
+	r := container.Resources{
+		Memory:   d.Config.Memory,
+		NanoCPUs: int64(d.Config.Cpu * math.Pow(10, 9)),
+	}
+	cc := container.Config{
+		Image:        d.Config.Image,
+		Tty:          false,
+		Env:          d.Config.Env,
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+	hc := container.HostConfig{
+		RestartPolicy:   rp,
+		Resources:       r,
+		PublishAllPorts: true,
+	}
+
+	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	err = d.Client.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := d.Client.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		return nil, err
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	return &DockerResult{
+		ContainerId: resp.ID,
+		Action:      "start",
+		Result:      "success",
+	}, nil
+}
+
+func (d *Docker) Stop(ctx context.Context, id string) (*DockerResult, error) {
+	err := d.Client.ContainerStop(ctx, id, container.StopOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = d.Client.ContainerRemove(ctx, id, types.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         false,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &DockerResult{
+		ContainerId: id,
+		Action:      "stop",
+		Result:      "success",
+	}, nil
+}
