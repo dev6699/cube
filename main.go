c22cb717e87d79723824a9bbefc333838a5ba069
kweijack <kweijack.dev@gmail.com>
2024-10-15 20:56:20 +0800

    manager send task to worker

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	"github.com/dev6699/cube/manager"
+	"github.com/dev6699/cube/task"
 	"github.com/dev6699/cube/worker"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -18,12 +22,56 @@ func main() {
 func run() error {
 	ctx := context.Background()
 	w := worker.New("worker-1")
-	api := worker.NewApi("127.0.0.1", 5555, w)
+
+	host := "127.0.0.1"
+	port := 5555
+	api := worker.NewApi(host, port, w)
 
 	go runTasks(ctx, w)
 	go w.CollectStats()
+	go api.Start()
 
-	return api.Start()
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(workers)
+
+	for i := 0; i < 3; i++ {
+		t := task.Task{
+			ID:    uuid.New(),
+			Name:  fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "postgres:12-alpine",
+			Env:   []string{"POSTGRES_USER=cube", "POSTGRES_PASSWORD=secret"},
+		}
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+		m.AddTask(te)
+		err := m.SendWork()
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	go func() {
+		for {
+			log.Println("Update tasks")
+			err := m.UpdateTasks()
+			if err != nil {
+				log.Println(err)
+			}
+			time.Sleep(15 * time.Second)
+		}
+	}()
+
+	for {
+		log.Println("Tasks state:")
+		for _, t := range m.TaskDb {
+			fmt.Printf("Task id: %s, state: %d\n", t.ID, t.State)
+		}
+		time.Sleep(15 * time.Second)
+	}
 }
 
 func runTasks(ctx context.Context, w *worker.Worker) {
--- manager/manager.go
+++ manager/manager.go
@@ -1,22 +1,142 @@
 package manager
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/dev6699/cube/queue"
 	"github.com/dev6699/cube/task"
+	"github.com/dev6699/cube/worker"
 	"github.com/google/uuid"
 )
 
 type Manager struct {
 	Pending       queue.Queue[task.TaskEvent]
-	TaskDb        map[string][]*task.Task
-	EventDb       map[string][]*task.TaskEvent
+	TaskDb        map[uuid.UUID]*task.Task
+	EventDb       map[uuid.UUID]*task.TaskEvent
 	Workers       []string
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+	LastWorker    int
 }
 
-func (m *Manager) SelectWorker() {}
+func New(workers []string) *Manager {
+	workerTaskMap := make(map[string][]uuid.UUID)
+	for _, worker := range workers {
+		workerTaskMap[worker] = []uuid.UUID{}
+	}
 
-func (m *Manager) UpdateTasks() {}
+	return &Manager{
+		Pending:       queue.Queue[task.TaskEvent]{},
+		Workers:       workers,
+		TaskDb:        make(map[uuid.UUID]*task.Task),
+		EventDb:       make(map[uuid.UUID]*task.TaskEvent),
+		TaskWorkerMap: make(map[uuid.UUID]string),
+		WorkerTaskMap: workerTaskMap,
+		LastWorker:    0,
+	}
+}
 
-func (m *Manager) SendWork() {}
+func (m *Manager) AddTask(te task.TaskEvent) {
+	m.Pending.Enqueue(te)
+}
+
+func (m *Manager) SelectWorker() string {
+	var newWorker int
+	if m.LastWorker+1 < len(m.Workers) {
+		newWorker = m.LastWorker + 1
+		m.LastWorker++
+	} else {
+		newWorker = 0
+		m.LastWorker = 0
+	}
+
+	return m.Workers[newWorker]
+}
+
+func (m *Manager) SendWork() error {
+	if m.Pending.Len() == 0 {
+		return nil
+	}
+
+	te, ok := m.Pending.Dequeue()
+	if !ok {
+		return nil
+	}
+
+	w := m.SelectWorker()
+	t := te.Task
+	m.EventDb[te.ID] = &te
+	m.WorkerTaskMap[w] = append(m.WorkerTaskMap[w], t.ID)
+	m.TaskWorkerMap[t.ID] = w
+
+	t.State = task.Scheduled
+	m.TaskDb[t.ID] = &t
+
+	data, err := json.Marshal(te)
+	if err != nil {
+		return err
+	}
+
+	url := fmt.Sprintf("http://%s/tasks", w)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		m.Pending.Enqueue(te)
+		return err
+	}
+
+	d := json.NewDecoder(resp.Body)
+	if resp.StatusCode != http.StatusCreated {
+		var e worker.ErrResponse
+		err := d.Decode(&e)
+		if err != nil {
+			return err
+		}
+	}
+
+	var createdTask task.Task
+	err = d.Decode(&createdTask)
+	if err != nil {
+		return err
+	}
+	log.Printf("task created %#v\n", t)
+	return nil
+}
+
+func (m *Manager) UpdateTasks() error {
+	for _, worker := range m.Workers {
+		url := fmt.Sprintf("http://%s/tasks", worker)
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("invalid status %v", resp.StatusCode)
+		}
+
+		d := json.NewDecoder(resp.Body)
+		var tasks []*task.Task
+		err = d.Decode(&tasks)
+		if err != nil {
+			return err
+		}
+
+		for _, t := range tasks {
+			_, ok := m.TaskDb[t.ID]
+			if !ok {
+				continue
+			}
+
+			m.TaskDb[t.ID].State = t.State
+			m.TaskDb[t.ID].StartTime = t.StartTime
+			m.TaskDb[t.ID].FinishTime = t.FinishTime
+			m.TaskDb[t.ID].ContainerID = t.ContainerID
+		}
+	}
+
+	return nil
+}
